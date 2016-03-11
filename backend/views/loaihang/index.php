a76d32e79fb990e8dc46e2772b9b06c3f7eaeafb
Tung <ithhhp@gmail.com>
2016-03-12 00:35:36 +0700

    đổi 4 file

--- backend/views/loaihang/index.php
+++ backend/views/loaihang/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\LoaihangSearch */
+/* @var $searchModel common\models\LoaihangSearchgggggg */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = Yii::t('app', 'Quản lý loại hàng');
