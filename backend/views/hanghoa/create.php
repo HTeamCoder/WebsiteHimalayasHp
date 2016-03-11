a29eced9e9b3bac6fbed529f3dcc1d184dfe61fc
thangtk2 <thangtk2@gmail.com>
2016-03-12 00:35:37 +0700

    push thư mục view hanghoa

---
+++ backend/views/hanghoa/create.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model common\models\hanghoa */
+
+$this->title = Yii::t('app', 'Create Hanghoa');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Hanghoas'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="hanghoa-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'anh'=>$anh,
+    ]) ?>
+
+</div>
