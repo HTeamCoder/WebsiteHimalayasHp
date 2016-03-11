fa47c5086a816822937c703c688a30b6b8556175
Tung <ithhhp@gmail.com>
2016-03-12 00:28:01 +0700

    thay đổi file create của loại hàng

--- backend/views/loaihang/create.php
+++ backend/views/loaihang/create.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model common\models\loaihang */
 
-$this->title = Yii::t('app', 'Create Loaihang');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Loaihangs'), 'url' => ['index']];
+$this->title = Yii::t('app', 'Tạo mới loại hàng');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Loại hàng'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="loaihang-create">
