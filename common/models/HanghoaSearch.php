55ac678786900c9a919dad87dc3dde2946d49668
thangtk2 <thangtk2@gmail.com>
2016-03-14 00:06:42 +0700

    update model hanghoasearch

--- common/models/HanghoaSearch.php
+++ common/models/HanghoaSearch.php
@@ -46,6 +46,9 @@ class HanghoaSearch extends hanghoa
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination' => [
+                'pageSize' => 2,
+            ],
         ]);
 
         $this->load($params);
