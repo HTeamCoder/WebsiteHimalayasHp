2ba09c3242968da7ec76612952822daeb1f1ead8
thangtk2 <thangtk2@gmail.com>
2016-04-20 00:04:26 +0700

    l

--- backend/views/hanghoa/_form.php
+++ backend/views/hanghoa/_form.php
@@ -5,7 +5,7 @@ use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\hanghoa */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\widgets\ActiveForm kk*/
 ?>
 
 <div class="hanghoa-form">
